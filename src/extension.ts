25f24a2d81be1c0f5a69b7b45d206ea0e78b91cf
Lampower <lampower.kirill@gmail.com>
2025-07-15 11:52:36 +0400

    add getting directories in workspace by searching

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,95 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('addquickfile.addQuickFile', async () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (!workspaceFolders) {
+			vscode.window.showErrorMessage('Открой рабочую папку (workspace), чтобы использовать эту команду.');
+			return;
+		}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "addquickfile" is now active!');
+		const rootPath = workspaceFolders[0].uri.fsPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('addquickfile.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from AddQuickFile!');
+		let input = '';
+		const quickPick = vscode.window.createQuickPick();
+		quickPick.placeholder = 'Введите часть пути файла или папки...';
+
+		
+		const updateItems = async (value: string) => {
+			input = value;
+			const results = await searchPaths(rootPath, value);
+			quickPick.items = results.map(r => ({ label: r }));
+		};
+		
+		updateItems("");
+
+		quickPick.onDidChangeValue(updateItems);
+
+		quickPick.onDidAccept(() => {
+			const selection = quickPick.selectedItems[0];
+			if (selection) {
+				vscode.window.showInformationMessage(`Вы выбрали: ${selection.label}`);
+			}
+			quickPick.hide();
+		});
+
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.show();
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+async function searchPaths(root: string, query: string): Promise<string[]> {
+	const results: string[] = [];
+
+	const searchWords = [".venv", "venv", "site-package", "node-modules"];
+
+	async function walk(dir: string) {
+		let files = await fs.promises.readdir(
+			dir,
+			// check this shit
+			{"withFileTypes": false, "recursive": false}
+		);
+		
+		let approvedFiles = [];
+
+		for (const file of files) {
+			let shouldAdd = true;
+			for (const word of searchWords) {
+				if (file.toLowerCase().includes(word)) {
+					shouldAdd = false;
+					break;
+				}
+			}
+			if (shouldAdd) {
+				approvedFiles.push(file);
+			}
+		}
+
+		for (const file of approvedFiles) {
+			const fullPath = path.join(dir, file);
+			const relPath = path.relative(root, fullPath);
+			const stat = await fs.promises.stat(fullPath);
+
+			if (stat.isDirectory()) {
+				if (relPath.toLowerCase().includes(query.toLowerCase())) {
+					results.push(relPath);
+				}
+				await walk(fullPath);
+			}
+		}
+	}
+
+	try {
+		await walk(root);
+	} catch (err) {
+		console.error('Ошибка при обходе папок:', err);
+	}
+
+	return results.slice(0, 50); // Ограничим кол-во результатов
+}
+
 export function deactivate() {}
